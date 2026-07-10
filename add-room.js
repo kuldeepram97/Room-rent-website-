b699af3d682345d559c41a247b65e2171d425a69
kuldeepram97 <kuldeepbains197@gmail.com>
2026-07-10 22:08:26 +0530

    Update add-room.js

--- add-room.js
+++ add-room.js
@@ -13,37 +13,4 @@ import {
 collection, 
 addDoc 
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-
-document.getElementById("roomForm").addEventListener("submit", async (e)=>{
-
-e.preventDefault();
-
-let title = document.getElementById("title").value;
-let location = document.getElementById("location").value;
-let rent = document.getElementById("rent").value;
-let description = document.getElementById("description").value;
-let image = document.getElementById("image").files[0];
-
-
-let imageRef = ref(storage, "rooms/" + image.name);
-
-await uploadBytes(imageRef, image);
-
-let imageUrl = await getDownloadURL(imageRef);
-
-
-await addDoc(collection(db,"rooms"),{
-title,
-location,
-rent,
-description,
-imageUrl
-});
-
-
-alert("Room Added Successfully");
-
-document.getElementById("roomForm").reset();
-
 });
